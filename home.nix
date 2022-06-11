1b97b68c192d894f267a1ef3bfceb5a36a0b9dd4
Andrew Rae <ajrae.nv@gmail.com>
2022-06-11 11:48:27 -0700

    Adding fonts.

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
 
   imports = [
     ./modules/alacritty.nix
+    ./modules/fonts.nix
     ./modules/zsh.nix
   ];
 
---
+++ modules/fonts.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+
+{
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+}
