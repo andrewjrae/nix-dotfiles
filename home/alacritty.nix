5d1f9c0986e2ecda0d15c4185d6561c2bf9ab897
Andrew Rae <ajrae.nv@gmail.com>
2023-03-14 09:03:27 -0400

    Making alacritty work with both linux and darwin.

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -1,8 +1,14 @@
 { config, lib, pkgs, ... }:
 
+let
+  alacrittyConfig = (if pkgs.stdenv.isDarwin
+                     then ../configs/alacritty/darwin.yml
+                     else ../configs/alacritty/linux.yml);
+in
 {
   home.packages = with pkgs; [
     alacritty
   ];
-  xdg.configFile."alacritty/alacritty.yml".source = ../configs/alacritty/alacritty.yml;
+  xdg.configFile."alacritty/base_config.yml".source = ../configs/alacritty/base_config.yml;
+  xdg.configFile."alacritty/alacritty.yml".source = alacrittyConfig;
 }
